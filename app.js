84c923ff251efc4ab5ee3872ed0ec70a15293901
Keiko <keitakeiko@hotmail.com>
2023-07-15 13:47:21 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得連線狀態
+const db = mongoose.connection
+// 連現異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () =>{
+  console.log('mongodb connected!')
+})
+
 
 // 設定首頁路由
 app.get('/', (req, res) => {
---
+++ models/expense.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const expenseSchema = new Schema({
+  name: {
+    type: String
+  },
+  amount: {
+    type: Number
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  categoryId: {
+    type: Schema.Types.objectId,
+    ref: 'Category',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Expense', expenseSchema)
