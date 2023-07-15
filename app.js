dc18e2bf7e8f87bb3c11c4b3dd30e9101deaa124
Keiko <keitakeiko@hotmail.com>
2023-07-15 14:54:32 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
+
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
 
 // 設定連線到 mongoDB
 mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
@@ -24,10 +27,17 @@ db.once('open', () =>{
 })
 
 
+// 建立樣版引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+// 啟用樣版引擎
+app.set('view engine', 'handlebars')
+
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hi~')
+  res.render('index')
 })
+
 
 // 設定 port 
 app.listen(port, () => {
