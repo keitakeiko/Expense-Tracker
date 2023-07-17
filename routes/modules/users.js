032c80ec0e3c474f80229ad44961159d1e6d202a
Keiko <keitakeiko@hotmail.com>
2023-07-18 00:07:46 +0800

    feat: add user login & register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-
+// login
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  res.render('login')
+})
+
+// register
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 
