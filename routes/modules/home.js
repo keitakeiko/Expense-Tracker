548a2526201cc41825fc10742b0d6414388559e7
Keiko <keitakeiko@hotmail.com>
2023-07-22 22:30:26 +0800

    add filter function in index.handlebars

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,25 +2,50 @@
 // 引用 express 與 express 路由器
 const express = require('express')
 const router = express.Router()
+const dayjs = require('dayjs')
+const localeData = require('dayjs/plugin/localeData')
 
 const Expense = require('../../models/expense') // 引入 expense model
 const Category = require('../../models/category') // 引入 category model
 
-
+dayjs.extend(localeData)
 
 // 設定首頁路由
 router.get('/', async(req, res) => {
+  let total_expense = 0
+  const expense = []
   const userId = req.user._id
-  const { categoryId } = req.query
+  const { viewByCategory, viewByMonth } = req.query
+  const filterBy = { userId }
+  const month = dayjs.months() // 叫出 1 - 12 月字串的陣列
   const category = await Category.find().lean()
-  await Expense.find({ userId }) // 取出 Expense model 裡的所有資料
-    .lean() // 把 MongoDB 的 Model 物件轉換成乾淨的 JS 資料陣列
-    .sort({ _id: 'asc' }) // 根據 _id 升冪排序
-    .then( expenses => {
-      res.render('index', { expenses, category, categoryId })}) // 將資料傳給 index 樣板
-    
-    
-    .catch(error => console.log(error)) // 錯誤處理
+
+  if (viewByCategory) {
+    filterBy.category = viewByCategory
+  }
+  if (viewByMonth) {
+    const selectMonth = dayjs.month(month.indexOf(viewByMonth))
+    filterBy.date = { // 設定搜尋整個月份，toDate()為 JS語法，可轉成系統可辨識格式
+      $gte: dayjs(selectMonth).startOf('month').toDate(),
+      $lte: dayjs(selectMonth).endOf('month').toDate()
+    }
+  }
+
+  const records = await Expense.find(filterBy).lean().sort({ date: 'asc'})
+
+  records.forEach(record => {
+    expense.push(record.amount)
+    let categories = category.find(category => record.category === category.name)
+    record.category_label = categories.label
+    record.category_icon = categories.category_icon
+    record.match_id = categories._id
+    record.date = dayjs(record.date).format('YYYY-MM-DD ddd')
+  })
+
+  if (expense.length !== 0) {
+    total_expense = expense.reduce((x, y) => x + y)
+  }
+  res.render('index', { records, total_expense, category, month, viewByCategory, viewByMonth})
 })
 
 // 匯出路由模組
